21d1454e110d86443ccea6ffa8697c7481e86276
rishisurana13 <rishisurana@hotmail.com>
2020-10-05 14:09:56 +0530

    try 2

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,25 @@
-#!/usr/bin/env sh
+# #!/usr/bin/env sh
 
-# abort on errors
-set -e
+# # abort on errors
+# set -e
 
-# build
-npm run build
+# # build
+# npm run build
 
-# navigate into the build output directory
-cd docs
+# # navigate into the build output directory
+# cd docs
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+# # if you are deploying to a custom domain
+# # echo 'www.example.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
+# git init
+# git add -A
+# git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:rishisurana13/rishisurana13.github.io.git main
+# # if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:rishisurana13/rishisurana13.github.io.git main
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# # if you are deploying to https://<USERNAME>.github.io/<REPO>
+# # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-cd -
+# cd -
